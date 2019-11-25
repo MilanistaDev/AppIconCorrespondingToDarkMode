0ce7d8a2a1ab5688dc8c78b2feb4677a74c653e5
Takuya Aso <milanista224@gmail.com>
2019-11-25 19:05:20 +0900

    Implemented AppIcon code

--- AppIconCorrespondingToDarkMode/ViewController.swift
+++ AppIconCorrespondingToDarkMode/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
 
+        if #available(iOS 13.0, *) {
+            if self.traitCollection.userInterfaceStyle == .dark {
+                // ダークモード用のアプリを設定する
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                    UIApplication.shared.setAlternateIconName("AppIcon-Dark") { error in
+                        if let error = error {
+                            self.errorLabel.text = error.localizedDescription
+                        }
+                    }
+                }
+            } else {
+                // nilをセットしデフォルトのアプリアイコン画像に変更
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                    UIApplication.shared.setAlternateIconName(nil) { error in
+                        if let error = error {
+                            self.errorLabel.text = error.localizedDescription
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-
