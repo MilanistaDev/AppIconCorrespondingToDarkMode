429ff40f5ebf85a746fd3a8b7fc9640d02ed3c29
Takuya Aso <milanista224@gmail.com>
2019-11-25 19:07:27 +0900

    Added settings class.

---
+++ AppIconCorrespondingToDarkMode/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  AppIconCorrespondingToDarkMode
+//
+//  Created by Takuya Aso on 2019/11/25.
+//  Copyright © 2019 Takuya Aso. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
